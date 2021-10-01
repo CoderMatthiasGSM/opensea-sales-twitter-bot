ad3f1bbae63d579d7c059ada3fd463ba3aa95c9d
CoderMatthiasGSM <87883303+CoderMatthiasGSM@users.noreply.github.com>
2021-09-30 23:50:12 -0400

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ function formatAndSendTweet(event) {
 // Poll OpenSea every minute & retrieve all sales for a given collection in the last minute
 // Then pass those events over to the formatter before tweeting
 setInterval(() => {
-    const lastMinute = moment().startOf('minute').subtract(600, "seconds").unix();
+    const lastMinute = moment().startOf('minute').subtract(598, "seconds").unix();
 
     axios.get('https://api.opensea.io/api/v1/events', {
         params: {
