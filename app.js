ca65e10348ab092df1c565625ede6708edb67aad
CoderMatthiasGSM <87883303+CoderMatthiasGSM@users.noreply.github.com>
2021-11-20 00:24:00 -0500

    Update app.js

--- app.js
+++ app.js
@@ -34,22 +34,15 @@ function formatAndSendTweet(event) {
 setInterval(() => {
     const lastMinute = moment().startOf('minute').subtract(299, "seconds").unix();
     
-    /*axios.get('https://api.opensea.io/api/v1/events', {
-        headers: {Accept: 'application/json', 'X-API-KEY': '7a3e6b0eafd24c31afe1c18f56bdcf93'}
+    axios.get('https://api.opensea.io/api/v1/events', {
         params: {
             collection_slug: process.env.OPENSEA_COLLECTION_SLUG,
             event_type: 'successful',
             occurred_after: lastMinute,
             only_opensea: 'false'
-        }
-    */   
-    const options = {
-    method: 'GET',
-    headers: {Accept: 'application/json', 'X-API-KEY': '7a3e6b0eafd24c31afe1c18f56bdcf93'}
-    };
-
-fetch('https://api.opensea.io/api/v1/events?collection_slug=process.env.OPENSEA_COLLECTION_SLUG&event_type=succesful&only_opensea=false&offset=0&limit=20&occurred_after=lastMinute', options)
-  .then(response => response.json())).then((response) => {
+        }, 
+        headers: {Accept: 'application/json', 'X-API-KEY': '7a3e6b0eafd24c31afe1c18f56bdcf93'}
+    }).then((response) => {
         const events = _.get(response, ['data', 'asset_events']);
 
         console.log(`${events.length} sales in the last 5 minutes...`);
